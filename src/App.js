4826df079d297d9f40895d6f93efc90e2db03e7b
Jose Montero Jr <josemariodev@gmail.com>
2019-07-10 14:43:21 -0500

    imported buttons and display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButtons/NumberButton';
+import Display from './components/DisplayComponents/Display';
+
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
-import Logo from "./components/DisplayComponents/Logo";
+import Logo from './components/DisplayComponents/Logo';
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -14,9 +17,11 @@ function App() {
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
   return (
-    <div className="container">
+    <div className='container'>
       <Logo />
-      <div className="App">
+      <div className='App'>
+        <NumberButton />
+        <Display />
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
       </div>
     </div>
