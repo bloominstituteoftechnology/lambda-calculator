dd7f66f9be62ef4eedba23ff6c91b42ec058fae0
vy3191 <venkyafhs@gmail.com>
2019-07-12 11:43:22 -0400

    Passed data to child component-NumberButton

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 //import any components needed
+import NumberButton from './NumberButton';
 import {numbers} from '../../../data';
 
 //Import your array data to from the provided data file
@@ -12,6 +13,7 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {numbers.map( (number) => <NumberButton key={index} number={number} />)}
     </div>
   );
 };
