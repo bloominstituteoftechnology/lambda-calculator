0ce999e38c15a067b0d4b42b71e0a5ed30e3bc7d
Joscelyn <JoscelynRose@gmail.com>
2019-07-16 16:07:20 -0400

    WIP: making number buttons

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from 'react';
 
 //import any components needed
 
-//Import your array data to from the provided data file
+// Question 1: Do I need to install sass? is something supposed to be displaying?
+// Question 2: Am I going about this the right way?
 
+
+//Import your array data to from the provided data file
+import { numbers } from './src/data.js';// Question 3: is this path name right? path relative to what?
+
+console.log(numbers);
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [numbers, setNumbers] = useState();
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+      { //STEP 3 - Use .map() to iterate over your array data and return a button
+       //component matching the name on the provided file. Pass
+       //it any props needed by the child component
+       
+      
+       const numberButtons = numbers.map(element => {
+         `<button>${element}</button>`
+       });
+       
+       
+       }
+
     </div>
   );
 };
