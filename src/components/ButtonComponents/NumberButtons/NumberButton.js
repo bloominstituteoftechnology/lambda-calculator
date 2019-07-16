b203fab86282d91a1dbf97be2fbd81ce4b9701d0
dangolbeeker <scottb305@outlook.com>
2019-07-16 19:21:57 -0400

    Restarted project couldnt find bug...

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
 };
+
+
+
+
+export default NumberButton
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
+import NumberButton from "./NumberButton"
 
 //Import your array data to from the provided data file
+import { numbers } from "./data.js"
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [currentNumbers, setCurrentNumbers] = useState( [numbers] )
   return (
+    console.log(numbers),
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+    {numbers.map((number) => { 
+        return (
+          <NumberButton key={number} number={number}/>
+      )
+      })}
     </div>
   );
 };
+
+export default Numbers
