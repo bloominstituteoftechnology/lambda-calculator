076558cab4c439058469b130acb374d3e9c97779
jazz-code <ren.meta@gmail.com>
2019-07-16 18:09:43 -0400

    map over array data returning button component

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import NumberButton from "./NumberButton.js";
-import { numbers } from "../../../data.js";
+import { numbers } from "../../../data";
+console.log(numbers);
 //import any components needed
 //Import your array data to from the provided data file
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
   const [numberState, setNumberState] = useState(numbers);
+  console.log("numbers, numberState");
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {numberState.map((number, index) => {
+        return <NumberButton number={number} key={index} />;
+      })}
     </div>
   );
 };
+
+export default Numbers;
