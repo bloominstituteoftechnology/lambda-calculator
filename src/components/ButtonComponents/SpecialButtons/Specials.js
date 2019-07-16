508f94890645e3842e8c0084415ba7004b8a45a9
tania <tanislavens@yahoo.com>
2019-07-16 18:56:37 -0400

    wip: all char present but unstyled

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -12,7 +12,7 @@ const Specials = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-      {specialState.map((special) => { return <SpecialButton special={special.char} /> })};
+      {specialState.map((special) => { return <SpecialButton special={special} /> })};
     </div>
   );
 };
