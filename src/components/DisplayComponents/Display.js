ff9ee145df2f27caafaa0cd1146592f6ddfbbe70
Mimi Hoang <miminhoang1@gmail.com>
2019-07-16 16:48:42 -0400

    ...

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./styles.module.css";
+import "./Display.module.css";
 
 const Display = (props) => {
   return(  
-  <div className={styles.display}>{props.data}</div>
+  <div className="display">{props.data}</div>
   );
 };
 export default Display;
