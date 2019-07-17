83ee71e6dd52398f7a30eb74e7596624f016213b
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-07-17 11:13:08 -0500

    still working on specials

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
+import Display from "./components/DisplayComponents/Display";
 import "./App.css";
 // STEP 4 - import the button and display components
+//{STEP 4 - Render your components here and be sure to properly import/export all files */}
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
@@ -12,15 +17,25 @@ function App() {
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
+const [current, setCurrent] = useState(0)
+const [total, setTotal] = useState(0)
+
+const changeData = input => {
+  setCurrent(input)
+}
 
   return (
-    <div className="container">
+    <div className="app-container">
       <Logo />
-      <div className="App">
-        {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <div>
+          <Display current={total}/>
+          <Specials total={changeData}/>
+          <Numbers total={changeData}/>
+        </div>
+        <Operators total={changeData}/>
       </div>
-    </div>
-  );
-}
+    );
+  }
+
 
 export default App;
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
+import OperatorButton from './OperatorButton'
 
+import {operators} from '../../../data';
 //import any components needed
 
 //Import your array data to from the provided data file
+/* {STEP 3 - Use .map() to iterate over your array data and return a button
+       component matching the name on the provided file. Pass
+       it any props needed by the child component}*/
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [ops, setOps] =useState(operators)
+
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+      {ops.map((item, index) => {
+        // console.log(item)
+        /*pass data to operator button*/
+        return <OperatorButton  key={index} operatorContent={item.char} value={item.value}/>
+      })}
     </div>
-  );
+  )
 };
+
+export default Operators
