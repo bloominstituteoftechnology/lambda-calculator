9c2c31a6696b731adc45dca8d53dc40a31d36a12
Brandon Desselle <desselle.brandon2@gmail.com>
2019-07-16 19:19:10 -0500

    Update NumberButton.js

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,37 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = ({ number }) => { // Passing {number} as props
+  let style_NumberButton;
+  if (number == 0) {
+    style_NumberButton = {
+      background: '#0E447E',
+      width: '180px',
+      color: '#fff',
+      border: 'none',
+      borderRadius: '40px',
+      boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.5), 0 6px 20px 0 rgba(0, 0, 0, 0.20)',
+      cursor: 'pointer',
+      outline: 'none',
+    }
+  } else {
+    style_NumberButton = {
+      background: '#0E447E',
+      color: '#fff',
+      border: 'none',
+      borderRadius: '40px',
+      boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.5), 0 6px 20px 0 rgba(0, 0, 0, 0.20)',
+      cursor: 'pointer',
+      outline: 'none',
+    }
+  }
+
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button className='clicky' style={style_NumberButton}>{number}</button>
     </>
   );
 };
+
+// Exporting the {NumberButton} component using the `export` keyword
+export default NumberButton;
