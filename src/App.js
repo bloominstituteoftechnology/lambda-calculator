3567fb30c2a02a9bff3b7f01c5893797ae0c1ff2
Ben <allenben@protonmail.com>
2019-07-17 10:25:03 -0500

    Added props within App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Numbers from './components/ButtonComponents/NumberButtons/Numbers.js';
 import Operators from './components/ButtonComponents/OperatorButtons/Operators.js';
@@ -10,7 +10,10 @@ import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
 import Logo from "./components/DisplayComponents/Logo";
 
 
-function App() {
+
+function App(props) {
+
+  const { firstValue, secondValue } = props;
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
@@ -21,6 +24,9 @@ function App() {
     <div className="container">
       
       <div className="App">
+        <div className="dataPlaceHolder">
+          0
+        </div>
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <div className='btnContainer'>
           <div className='col1'>
