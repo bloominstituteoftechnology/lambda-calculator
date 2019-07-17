2c14a72da42f82b0877e6cc98e8e61527d15087b
Brandon Desselle <desselle.brandon2@gmail.com>
2019-07-16 19:19:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import "./App.css";
+import Display from './components/DisplayComponents/Display'; // Importing default export of the {Display} component
+import Buttons from './components/DisplayComponents/Buttons'; // Importing default export of the {Buttons} component
 // STEP 4 - import the button and display components
-// Don't forget to import any extra css/scss files you build into the correct component
-
-// Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 
 function App() {
@@ -12,12 +11,32 @@ function App() {
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
+  const style_Container = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+
+  const style_App = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    height: '725px',
+    width: '450px',
+    borderRadius: '15px',
+    backgroundColor: '#A9203C',
+  }
 
   return (
-    <div className="container">
-      <Logo />
-      <div className="App">
+    <div
+      className="container"
+      style={style_Container}>
+      <div
+        className="App"
+        style={style_App}>
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Logo />
+        <Display />
+        <Buttons />
       </div>
     </div>
   );
