c658a5ea514e0c9d26772d3a7577a40480ede926
Scott Renz <scottrenz@scottrenz.com>
2019-07-20 21:51:27 -0400

    made it calculate the expresion first before doing the x/- special

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -15,7 +15,9 @@ const SpecialButton = props => {
   return (
     <button onClick={ ()=> props.setDataSB( (props.specialSB === 'C') ? '' : ( (props.specialSB === '%') ?
      tryit()*.01
-     : -parseFloat(props.dispDataSB) ))}  style={btnStyle}>{props.specialSB}</button>
+     :
+      -tryit()
+      ))}  style={btnStyle}>{props.specialSB}</button>
 
         /* Display a button element rendering the data being passed down from the parent container on props */
   );
