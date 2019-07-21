c97d247e2be2415ad8ca1b55bcb5a3624e3bd959
Scott Renz <scottrenz@scottrenz.com>
2019-07-20 20:35:07 -0400

    got operator buttons working

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,10 +1,13 @@
 import React from "react";
+import {btnStyle} from './Operators'
 
-const OperatorButton = () => {
+const OperatorButton = props => {
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
+    
+    <button onClick={ ()=> props.setDataOB( (props.operatorOB.char === '=')  ? eval(props.dispDataOB) : props.dispDataOB + props.operatorOB.value) }  style={btnStyle}>{props.operatorOB.char}</button>
+
+    // {/* Display a button element rendering the data being passed down from the parent container on props */}
+);
 };
 export default OperatorButton;
+
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,11 +1,11 @@
 
 import React from "react";
 import {operators } from "../../../data"
-
+import OperatorButton from './OperatorButton'
 //import any components needed
 
 //Import your array data to from the provided data file
-const btnStyle = {
+export const btnStyle = {
   width: '80px',
   height: '0',
   paddingBottom: '80px',
@@ -18,16 +18,16 @@ const btnStyle = {
   color: 'white',    
 }
 
-const Operators = () => {
+const Operators = props => {
   // STEP 2 - add the imported data to state
-  // const [operators, setSpecial] = useState(operators);
   return (
     <div>
       {
-          operators.map((item, index) => (
-          <button style={btnStyle}>{item.char}</button>
-        ))
-                /* STEP 3 - Use .map() to iterate over your array data and return a button
+                  operators.map((operator) => (
+                    <OperatorButton operatorOB={operator} dispDataOB={props.dispDataO} setDataOB={props.setDataO}/>
+                    //  <button onClick={ ()=> {props.props(item) }}  style={item === '0' ? btn0Style : btnStyle}>{item}</button>
+                    ))      
+        /* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
     </div>
