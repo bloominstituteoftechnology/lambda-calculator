1564c3fcdbccd3dd77e6a38f1fd378bd4c749d94
Scott Renz <50181315+scottrenz@users.noreply.github.com>
2019-07-21 23:30:33 -0400

    Update SpecialButton.js

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -2,6 +2,7 @@ import React from "react";
 import {btnStyle} from "./Specials";
 
 const SpecialButton = props => {
+  
   function tryit (){
     let x ='';
       try {
@@ -13,14 +14,28 @@ const SpecialButton = props => {
     }
     
   return (
-    <button onClick={ ()=> props.setDataSB( (props.specialSB === 'C') ? '' : ( (props.specialSB === '%') ?
-     tryit()*.01
-     :
-      -tryit()
-      ))}  style={btnStyle}>{props.specialSB}</button>
-
+      (props.specialSB === 'M=' )
+      ?
+(
+      <button onClick={ ()=> props.setMem(props.dispDataSB)}  style={btnStyle}>{props.specialSB}</button>
+)
+:
+(
+<button onClick={ ()=> props.setDataSB( (props.specialSB === 'C') ? ''
+ :
+(props.specialSB === 'MR')
+?
+(props.dispDataSB + props.memData)
+:
+( (props.specialSB === '%') ?
+tryit()*.01
+:
+ -tryit()
+ ))}  style={btnStyle}>{props.specialSB}</button>
+  )
         /* Display a button element rendering the data being passed down from the parent container on props */
   );
+
 };
 
 export default SpecialButton;
