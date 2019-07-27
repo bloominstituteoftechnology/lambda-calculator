7dae3c34ec70b40223d82ff1e27eddb8cdb8fe0c
Ricardo Barbosa <ric3639@gmail.com>
2019-07-27 10:10:53 -0700

    Calculator rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
-
+import Numbers from './components/ButtonComponents/NumberButtons/Numbers'
+import Operators from './components/ButtonComponents/OperatorButtons/Operators'
+import Specials from './components/ButtonComponents/SpecialButtons/Specials'
 
  
 
@@ -9,6 +11,7 @@ import "./App.css";
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import Display from './components/DisplayComponents/Display'
 
 function App() {
  
@@ -23,9 +26,13 @@ function App() {
   return (
     <div className="container">
       <Logo />
+      <div className = 'display'><Display /></div>
       <div className="App">
         {/* STEP 3 - Render your components here and be sure to properly import/export all files */}
-        
+        <Numbers />
+        <Operators />
+        <Specials />
+
         
       </div>
     </div>
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
+  const {specials} = props;
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button>{specials}</button>
     </>
   );
 };
+export default SpecialButton;
