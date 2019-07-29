0475bdf160b513e873bd7a2a5cca4fa5c7f8ca46
Robert Thompson <rthom2171@icloud.com>
2019-07-28 20:37:36 -0500

    Began styling components with scss/sass

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,7 @@
 import React from "react";
 
 const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+  return <div>{/* Display any props data here */}Test</div>;
 };
+
+export default Display;
