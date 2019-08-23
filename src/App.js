0cf1130b20ae5ffb206b8afc0df2ca56610d15c2
YasirHasn9 <hassanyasir223@gmail.com>
2019-08-22 23:15:14 -0700

    display spcilas  on the screen

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials"
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 
@@ -19,6 +20,7 @@ function App() {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Numbers />
+        <Specials />
       </div>
     </div>
   );
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button>{props.special}</button>
     </>
   );
 };
+export default SpecialButton;
