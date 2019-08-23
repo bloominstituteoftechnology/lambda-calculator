75ffd3726a8efeeaa1112e0a1f547a33a6471514
YasirHasn9 <hassanyasir223@gmail.com>
2019-08-23 01:33:16 -0700

    finished styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
 import Specials from "./components/ButtonComponents/SpecialButtons/Specials"
 import Operators from "./components/ButtonComponents/OperatorButtons/Operators"
+import Display from "./components/DisplayComponents/Display"
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 
@@ -17,12 +18,25 @@ function App() {
 
   return (
     <div className="container">
-      <Logo />
+    <div className="logo">
+    <Logo />
+    </div>
+    <div className="dispaly">
+    <Display />
+    </div>
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-        <Numbers />
-        <Operators />
-        <Specials />
+          <div className="numAndSpc">
+              <div className="specials">
+                  <Specials />
+              </div>
+              <div className="numbers">
+                  <Numbers />
+              </div>
+          </div>
+          <div className="operators">
+              <Operators />
+          </div>
       </div>
     </div>
   );
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,8 @@
 import React from "react";
 
 const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+  return <div className="dispaly">
+    <span>{0}</span>
+  </div>;
 };
+export default Display;
