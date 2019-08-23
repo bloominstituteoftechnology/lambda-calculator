dfaeb9cd3449def60b5bcf12aae632cd73b6077f
Clif <clifhodges13@gmail.com>
2019-08-22 20:38:47 -0500

    exported the data arrays. working on getting the number buttons rendered with a .map()

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
+  const { nums } = props
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button>{nums}</button>
     </>
   );
 };
+
+export default NumberButton
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
+import NumberButton from './NumberButton';
 
 //Import your array data to from the provided data file
+import { numbers } from '../../../data';
 
-const Numbers = () => {
+
+const Numbers = (props) => {
+  const { numbers } = props;
+
   // STEP 2 - add the imported data to state
+  const [numberState, setNumberState] = useState(numbers)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {numbers.map((item, index) => {
+         return <NumberButton key={index} numbers={item.numberState} />
+       })}
+       <CreateNumberButtons numbers={numberState} />
     </div>
   );
 };
+
+const CreateNumberButtons = (numbers) => {
+  numbers.map(() => {
+    return <NumberButton />
+  })
+}
+
+export default Numbers;
