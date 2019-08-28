af53edd4b2b3326595d9794b1442692961ce6c95
Ariel Rodriguez <ariel_rodriguez@Ariels-Mac-mini.local>
2019-08-27 21:44:18 -0400

    changes to styling

--- src/App.js
+++ src/App.js
@@ -23,9 +23,12 @@ function App() {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Display number={416}/>
+        <Specials />
         <Numbers />
         <Operators />
-        <Specials />
+        
+        
+        
       </div>
     </div>
   );
--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const OperatorButton = (props) => {
   //console.log(props)
   return (
-    <button className='button-style'>
+    <button className='button-style button-layout'>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       {props.operatorBtn.value}
     </button>
