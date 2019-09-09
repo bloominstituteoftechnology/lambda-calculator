54a3da7b2d90d8171c24f395e10df3a3970b4b30
Christina <chmagorton@gmail.com>
2019-09-09 13:48:50 -0600

    Adding class to go with added display css

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+  return <div className="display">{/* Display any props data here */}</div>;
 };
