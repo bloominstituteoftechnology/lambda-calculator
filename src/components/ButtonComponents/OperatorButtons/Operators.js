b148809abd6a382710f9e8b6ea7d970cb8e180d2
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2019-09-10 15:22:22 -0400

    mvp complete

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import OperatorButton from "./OperatorButton";
+import { operators } from "../../../data";
 
 //import any components needed
 
@@ -6,11 +8,18 @@ import React from "react";
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+
+  const [opt] = useState(operators);
   return (
-    <div>
+    <div className="operators">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {opt.map((data, idx) => (
+        <OperatorButton key={idx} operatorData={data.char} />
+      ))}
     </div>
   );
 };
+
+export default Operators;
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
+import SpecialButton from "./SpecialButton";
 
 //Import your array data to from the provided data file
+import { specials } from "../../../data";
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
+  const [spe] = useState(specials);
 
   return (
-    <div>
+    <div className="specials">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {spe.map((el, idx) => (
+        <SpecialButton key={idx} specialData={el} />
+      ))}
     </div>
   );
 };
+
+export default Specials;
