4e1c951f065ba454fc4a3e9442b359e23915e94a
Steve Renner <che1111@hotmail.com>
2019-09-10 13:27:25 -0700

    Operators added, debugging

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import "./App.css";
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+import OperatorButton from './OperatorButton.js'
+import {operators} from '../../../../src/data.js'
 
 //import any components needed
 
 //Import your array data to from the provided data file
 
-const Operators = () => {
+const Operators = (props) => {
+  const [operator, setOperator] = useState(operators)
   // STEP 2 - add the imported data to state
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
+      {operator.map(calcOperator =>{
+        return(
+          <OperatorButton key={calcOperator.char} operator={calcOperator} addOperator = {props.addOperator}/>/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+         ) 
+      })}
     </div>
   );
 };
+
+export default Operators;
