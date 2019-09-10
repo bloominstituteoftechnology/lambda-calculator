e2571b53802030a55c3fa8e706df2e903f4dc131
Christina <chmagorton@gmail.com>
2019-09-10 06:17:03 -0600

    Adding an import example

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 //import any components needed
-
+// example of import from data.js. Note all the ../   This is how we move through folders. 
+/* 
+import { numbers } from '../../../data' 
+*/
 //Import your array data to from the provided data file
 
 const Numbers = () => {
