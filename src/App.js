b198e293f957adfafd2ae896a9124dd202010607
meera-andersen <meera.andersen@gmail.com>
2019-09-10 14:44:28 -0600

    added special buttons

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import "./App.css";
 import Logo from "./components/DisplayComponents/Logo";
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers"
 import Operators from "./components/ButtonComponents/OperatorButtons/Operators"
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials"
+
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
@@ -21,6 +23,7 @@ function App() {
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Numbers />
         <Operators />
+        <Specials />
         
       </div>
     </div>
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button className = "special_button" >
+        <span role = "special button" aria-label = "special button">
+          {props.specials}
+        </span>
+      </button>
     </>
   );
 };
+
+export default SpecialButton;
