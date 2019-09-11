5e69e9d22fe91f6e8f2c22767458f275b48617d3
admin <admin@admins-MacBook-Pro.local>
2019-09-10 20:33:54 -0700

    mvp complete

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -3,15 +3,26 @@ import React from "react";
 const NumberButton = ({ input, number}) => {
 
 
-  
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className="numberButton" key={number} onClick={() => input(number)}  >
-        {number}
-      </button>
-    </>
-  );
+
+  if (number === "0"){
+    return (
+      <>
+        {/* Display a button element rendering the data being passed down from the parent container on props */}
+        <button className="numberButton zero" key={number} onClick={() => input(number)}  >
+          {number}
+        </button>
+      </>
+    )
+  } else{
+    return (
+      <>
+        {/* Display a button element rendering the data being passed down from the parent container on props */}
+        <button className="numberButton" key={number} onClick={() => input(number)}  >
+          {number}
+        </button>
+      </>
+    );
+  } 
 };
 
 export default NumberButton;
