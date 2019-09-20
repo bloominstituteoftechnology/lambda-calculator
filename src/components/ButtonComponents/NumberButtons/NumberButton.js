9046cbb0b89163a3212f85c640d461e0a780e91e
Dee Barizo <deebarizo@gmail.com>
2019-09-19 22:35:21 -0500

    Add styling to numbers

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -2,10 +2,14 @@ import React from "react";
 
 const NumberButton = props => {
   return (
-    <>
+    <React.Fragment>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button>{props.number}</button>
-    </>
+      {props.number !== "0" ? (
+        <button>{props.number}</button>
+      ) : (
+        <button className="zero">{props.number}</button>
+      )}
+    </React.Fragment>
   );
 };
 
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -11,7 +11,7 @@ const Operators = () => {
   const [operatorState, setOperatorState] = useState(operators);
 
   return (
-    <div>
+    <div className="operators">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/
