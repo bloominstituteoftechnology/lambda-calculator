9f862e930c3f1e6771f0d4959581a925729d448a
Ricardo Barbosa <ric3639@gmail.com>
2019-09-23 10:12:21 -0700

    Changes to Numbers.js

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 
 //import any components needed
-import NumberButton from './NumberButton'
+import NumberButton from "./NumberButton.js"
+
+
 //Import your array data to from the provided data file
-import { numbers } from './../../../data'
+import {numbers} from "./../../../data.js"
 const Numbers = () => {
   // STEP 2 - add the imported data to state
   const [numberState] = useState(numbers)
@@ -14,7 +16,7 @@ const Numbers = () => {
         it any props needed by the child component*/}
       {
         numberState.map((number, index) => (
-          <NumberButton numbers={number} key={index} />
+          <NumberButton numbers = {number} key = {index} />
         ))
       }
       
