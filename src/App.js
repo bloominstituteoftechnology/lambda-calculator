c6dcd3b94f2555cebfc31d9ae07f4bd9f90207e5
grizzlybrodams <jddaley328@gmail.com$4josh@josh-pcgit config --global user.namegit config --global user.name grizzlybrodams>
2019-09-22 20:48:27 -0600

    this is what ive done so far

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./App.css";
+import {Numbers} from './components/ButtonComponents/NumberButtons/Numbers';
+import {Operators} from './components/ButtonComponents/OperatorButtons/Operators';
+import {Specials} from './components/ButtonComponents/SpecialButtons/Specials';
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -18,6 +21,9 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Numbers />
+        <Operators/>
+        <Specials/>
       </div>
     </div>
   );
