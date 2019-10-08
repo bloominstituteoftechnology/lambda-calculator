751cc81cd99aa93f62e652bb5e6e88a26c8e80fc
Kerri-Ann <kaybee1jam@yahoo.com>
2019-10-08 17:16:21 -0400

    set up operator buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
+import Operators from './components/ButtonComponents/OperatorButtons/Operators';
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -20,6 +21,7 @@ function App() {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Numbers />
+        <Operators />
       </div>
     </div>
   );
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import { operators } from '../../../data';
-//import any components needed
-
-//Import your array data to from the provided data file
+import OperatorButton from './OperatorButton';
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [theOperators, setOperator] = useState(operators);
+  
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {theOperators.map((operator, index) => (
+         < OperatorButton key={index} operator={operator.char}/ >
+       ))}
     </div>
   );
 };
+
+export default Operators;
