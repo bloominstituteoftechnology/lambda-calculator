892cef750ebfe404605878c3484c08d9e0be25ca
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-08 15:34:27 -0700

    Moved button positioning, updated styles

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ function App() {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Specials />
-        <Numbers setNumberDisplay={setNumberDisplay} />
         <Operators />
+        <Numbers setNumberDisplay={setNumberDisplay} />
       </div>
     </div>
   );
