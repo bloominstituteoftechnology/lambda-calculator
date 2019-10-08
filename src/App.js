bf32ae3a1d5bd7fc16fddb6bd5a34e04490c0663
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-08 11:44:29 -0700

    Added display Component to App

--- src/App.js
+++ src/App.js
@@ -3,13 +3,8 @@ import "./App.css";
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
 import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
 import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
-
-// STEP 4 - import the button and display components
-// Don't forget to import any extra css/scss files you build into the correct component
-
-// Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
-import { operators } from "./data";
+import Display from "./components/DisplayComponents/Display";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -21,6 +16,7 @@ function App() {
   return (
     <div className="container">
       <Logo />
+      <Display />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Numbers />
