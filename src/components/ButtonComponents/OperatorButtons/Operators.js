6e7bd49a0ae9e84939837581d64e1760724daf2a
itsericfig <itsericfig@gmail.com>
2019-10-09 10:08:11 -0500

    Rendered buttons onto calculator, no functionality though

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,25 @@
-import React from "react";
-
+import React, { useState } from "react";
+import OperatorButton from "./OperatorButton";
 //import any components needed
+import { operators } from "../../../data";
 
 //Import your array data to from the provided data file
 
 const Operators = () => {
-  // STEP 2 - add the imported data to state
-  return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
+    // STEP 2 - add the imported data to state
+
+    const [operatorsState, setOperatorsState] = useState(operators);
+
+    return (
+        <div>
+            {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-    </div>
-  );
+            {operatorsState.map((element, index) => (
+                <OperatorButton key={index} operator={element.char} />
+            ))}
+        </div>
+    );
 };
+
+export default Operators;
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,17 +1,21 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { specials } from "../../../data";
 //import any components needed
-
+import SpecialButton from "./SpecialButton";
 //Import your array data to from the provided data file
 
 const Specials = () => {
-  // STEP 2 - add the imported data to state
-
-  return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
+    // STEP 2 - add the imported data to state
+    const [special, setSpecial] = useState(specials);
+    return (
+        <div>
+            {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-    </div>
-  );
+            {special.map((element, index) => (
+                <SpecialButton key={index} special={element} />
+            ))}
+        </div>
+    );
 };
+export default Specials;
