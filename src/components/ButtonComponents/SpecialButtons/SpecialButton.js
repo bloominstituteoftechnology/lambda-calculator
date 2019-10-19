a01e2095ef50ed00e5bd6602e2921c9898e6465b
Samuel Kraus <S_read@aol.com>
2019-10-19 16:22:21 -0500

    finished up OperatorButton

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = props => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
