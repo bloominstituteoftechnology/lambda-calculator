dfa347285c2b743d67ea38d70e74e56ac4493273
Samuel Kraus <S_read@aol.com>
2019-10-19 16:28:51 -0500

    completed Speicla buttons

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -4,6 +4,10 @@ const SpecialButton = props => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+    <button className="button">{props.button}</button>
     </>
   );
 };
+
+
+export default SpecialButton;
