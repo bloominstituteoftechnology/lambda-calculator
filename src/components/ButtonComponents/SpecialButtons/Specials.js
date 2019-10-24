e9e22cac3e665015586a6e0e42e99ac80a86c06f
Trevor Thompson <trevthom18@gmail.com>
2019-10-24 19:42:19 -0400

    Added map for all of the components that need a map

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,5 +1,6 @@
-import React from "react";
-import SpecialButton from "./SpecialButton"
+import React, {useState} from "react";
+import SpecialButton from "./SpecialButton";
+import { specials } from "../../../data.js";
 
 //import any components needed
 
@@ -7,12 +8,18 @@ import SpecialButton from "./SpecialButton"
 
 const Specials = props => {
   // STEP 2 - add the imported data to state
-
+  const [especial, setEspecial] = useState(specials);
+  console.log(specials);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {
+         especial.map(es => (
+
+          <SpecialButton es={es}/>)
+         )}
     </div>
   );
 };
