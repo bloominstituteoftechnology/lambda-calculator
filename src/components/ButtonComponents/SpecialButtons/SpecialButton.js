2b4f66e9091a48428763f62f62c6f7d706961dae
Robert Gordon <Komeuppance@gmail.com>
2019-11-05 13:40:31 -0800

    finished MVP

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { tsPropertySignature } from "@babel/types";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
   return (
     <>
+      <button>{props.button}</button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
 };
+
+export default SpecialButton
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
+import {specials} from "../../../data";
+import SpecialButton from "./SpecialButton"
 
 //import any components needed
 
@@ -6,12 +8,19 @@ import React from "react";
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
-
+  const [specialState] = useState(specials)
   return (
     <div>
+      <div className="specials-containter">
+      {specialState.map((number, index) => (
+        <SpecialButton key={index} button={number} />
+      ))}
+      </div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
     </div>
   );
 };
+
+export default Specials
