d68cfe1d0d45dfca04997fe4ab2ffbdda5e1e153
Robert Gordon <Komeuppance@gmail.com>
2019-11-05 12:48:12 -0800

    step 1-3 complete

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react";
+import {numbers} from "../../../data";
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
@@ -8,12 +9,20 @@ import { numbers } from '../../../data'
 //Import your array data to from the provided data file
 
 const Numbers = () => {
+  const [numberState] = useState(numbers)
   // STEP 2 - add the imported data to state
   return (
     <div>
+      <div className="numbers-containter">
+      {numberState.map((number, index) => (
+        <Numbers key={index} button={number} />
+      ))}
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       </div>
     </div>
   );
 };
+
+export default Numbers
