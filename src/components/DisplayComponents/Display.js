65f207a8f8f6b865850b2889c14e3338e3388799
Robby <robby-olson11@gmail.com>
2019-11-05 12:39:38 -0800

    display component done

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from 'react';
 
 const Display = () => {
-  return <div className="display">{/* Display any props data here */}</div>;
+  const [displayState, setDisplayState] = useState(0);
+  return <div className='display'>{displayState}</div>;
 };
+
+export default Display;
