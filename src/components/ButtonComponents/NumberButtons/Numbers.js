ec50a1b9e93ae32ef8d7e5f8b3f5e0d50cd744c1
oredola <oredolaolamide@gmail.com>
2019-11-06 23:52:14 +0100

    Number buttons added to DOM

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { numbers } from '../../../data';
+import NumberButton from "./NumberButton";
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
@@ -14,6 +16,18 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {
+        numbers.map((number, index) => {
+          return (
+          <NumberButton 
+          number={number}
+          key={index}
+          />
+          )
+        })
+      }
     </div>
   );
 };
+
+export default Numbers;
