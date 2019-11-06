57fc078bddea99a9743de67fe2af87f7d9b9533f
Anthony Lopez <anthonybeconscious@gmail.com>
2019-11-05 19:19:31 -0600

    fixing some stuff

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -5,10 +5,10 @@ import OperatorButton from "../OperatorButtons/OperatorButton"
 //import any components needed
 
 //Import your array data to from the provided data file
-const [operatorState] = useState(operators)
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [operatorState] = useState(operators)
   return (
     <div>
       {operatorState.map((ops, index) =>(
