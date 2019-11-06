234d92f958d969d3ebd123c7d60eaa239ea3130c
Ana Araujo <actanaaraujo@gmail.com>
2019-11-05 22:52:59 -0800

    adding files but its not working

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { operators } from "../../../data";
+import OperatorButton from "./OperatorButton";
+
 
 //import any components needed
 
 //Import your array data to from the provided data file
 
-const Operators = () => {
-  // STEP 2 - add the imported data to state
+const Operators = (props) => {
+  const [operatorState, setOperatorState] = useState(operators);
   return (
     <div>
+    {operatorState.map ((button, index)=>(
+      <OperatorButton
+      button={button}
+      key={index}
+      />
+    ))}
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
