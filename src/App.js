eb849df32945a2a229048e84f34fef70f11181ed
Your Real Name Here <Your Email Address Here>
2019-11-19 20:40:15 -0700

    Copied version. Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "./App.css";
-// STEP 4 - import the button and display components
-// Don't forget to import any extra css/scss files you build into the correct component
-
-// Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -16,6 +15,10 @@ function App() {
   return (
     <div className="container">
       <Logo />
+      <Numbers />
+      <Operators />
+      <Specials />
+
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
       </div>
@@ -24,3 +27,8 @@ function App() {
 }
 
 export default App;
+
+// STEP 4 - import the button and display components
+// Don't forget to import any extra css/scss files you build into the correct component
+
+// Logo has already been provided for you. Do the same for the remaining components
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
+import {specials} from "../../../data";
+import SpecialButton from "./SpecialButton";
 
 //import any components needed
 
@@ -6,12 +8,21 @@ import React from "react";
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
+  const [specialState, setSpecialState] = useState(specials);
 
   return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+    <div className = "specials_button">
+      {specialState.map((special,index) => 
+      <SpecialButton
+        aSpecial = {index}
+        bSpecial = {special}
+      />
+      )}
     </div>
   );
 };
+
+export default Specials;
+/* STEP 3 - Use .map() to iterate over your array data and return a button
+       component matching the name on the provided file. Pass
+       it any props needed by the child component*/a
