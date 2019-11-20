ab821487ac743552daaf36e8e206a6918715a5a7
Your Real Name Here <Your Email Address Here>
2019-11-19 20:43:50 -0700

    Fixed a bug

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -25,4 +25,4 @@ const Specials = () => {
 export default Specials;
 /* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/a
+       it any props needed by the child component*/
