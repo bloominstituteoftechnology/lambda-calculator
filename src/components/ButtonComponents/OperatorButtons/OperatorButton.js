561deb1232bbd5fceab3865c0c67d40447544b7e
Ian Fragoso <IanPaul@Ians-MacBook-Pro.local>
2019-11-19 19:53:20 -0700

    MVP complete

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -2,13 +2,10 @@ import React from "react";
 
 const OperatorButton = props => {
   return (
-    <>
     <button
        className="operator-button"
-       onClick={() => props.addOperator(props.operatorText.value)}
-     ></button>;
-      {props.operatorText.value}
-     </>
+       onClick={() => props.addOperator(props.operatorText.value)}>
+         {props.operatorText.value}</button>
   );
 };
 export default OperatorButton;
