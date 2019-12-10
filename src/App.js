8a508774caa0e9c0d2c844f3d641bf940caa8c09
skooger <adamjskooglambda@gmail.com>
2019-12-10 13:58:31 -0800

    finished the numbers component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import NumberButton from "./components/ButtonComponents/NumberButtons/Numbers";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -17,6 +18,7 @@ function App() {
     <div className="container">
       <Logo />
       <div className="App">
+        <NumberButton />
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
       </div>
     </div>
--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
+  console.log(props)
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    <button>
+    <span> 
+       
+      {props.button}
+    </span>
+      
+    </button>
   );
 };
+
+export default NumberButton;
