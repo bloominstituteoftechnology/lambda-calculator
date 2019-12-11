9a464bc03ed39d460ee7b80a20e2d9d30d8da218
arashhaji <56742302+arashhaji@users.noreply.github.com>
2019-12-10 19:27:22 -0500

    My buttons are not clicking

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const NumberButton = (props) => {
   return (
    
-    <button className="button number" onclick = { () => {
+    <button className="button Number" onclick = { () => {
       props.set(props.total + props.button)
     }}>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
