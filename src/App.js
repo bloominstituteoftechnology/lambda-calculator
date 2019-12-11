e9c6ea9f36e0e49ddd9c28ee779a12b03e8e4d3a
CorellCB <corellcb@gmail.com>
2019-12-11 09:48:52 -0500

    added operator buttons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
+
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 
@@ -20,6 +22,7 @@ function App() {
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
       
         <Numbers />
+        <Operators />
 
       </div>
     </div>
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { operators } from "../../../data";
+import OperatorButtons from "./OperatorButton";
 
 //import any components needed
 
@@ -6,11 +8,21 @@ import React from "react";
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const { op } = useState(operators);
+  
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+       
+        operators.map((e, i) => (
+          <OperatorButtons key={i} op={e.char} />
+        ))
+       
+       }
     </div>
   );
 };
+
+export default Operators;
