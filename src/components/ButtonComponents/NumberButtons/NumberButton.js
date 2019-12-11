2264848fdecc52780cd10f63e4d7e953abd2f5b6
CorellCB <corellcb@gmail.com>
2019-12-11 09:29:10 -0500

    added number buttons

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React from "react"; 
+import { tsPropertySignature } from "@babel/types";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    <button>
+      {/* Display a button element rendering the data being passed down from the parent container on props */
+      props.num
+      }
+    </button>
   );
 };
+
+export default NumberButton;
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
+import { numbers } from "../../../data";
+import NumberButton from "./NumberButton";
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
@@ -9,11 +11,24 @@ import { numbers } from '../../../data'
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const {num} = useState(numbers);
+  console.log(num);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+       
+        numbers.map((e, index) => (
+          <NumberButton num={e} key={index} />
+        ))
+          
+        
+
+       }
     </div>
   );
 };
+
+
+export default Numbers;
