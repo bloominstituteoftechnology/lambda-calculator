bbed757ecb80b7127d99b73d3488fb553c2f9579
Tyler Alsop <tylerjalsop@gmail.com>
2019-12-19 19:54:49 -0700

    Finished step 2. I think

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -10,6 +10,8 @@ import { numbers } from '../../../data'
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [numbersState, setNumbersState] = useState(numbers);
+  const [operatorsState, setOperators]
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
