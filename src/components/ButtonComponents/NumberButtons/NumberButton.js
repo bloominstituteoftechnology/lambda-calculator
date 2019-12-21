560dffe36a29e3dc8ef58f4ddb282063bfc7274e
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2019-12-21 12:00:09 -0500

    debug Me

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const NumberButton = props => {
   return (
     <>
-<button>{props.number}</button>
+<button> {props.number} </button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = props => {
   return (
     <>
+<button>{props.operator.char}</button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
 };
+
+export default OperatorButton;
