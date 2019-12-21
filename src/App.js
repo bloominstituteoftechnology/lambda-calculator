767ca5d7f5150bdbcb2977e815c919c37d6e228b
Tyler Alsop <tylerjalsop@gmail.com>
2019-12-20 19:11:10 -0700

    Moved the SpecialButton component above the NumberButton component

--- src/App.js
+++ src/App.js
@@ -29,9 +29,10 @@ function App() {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Display />
+        <SpecialButton />
         <NumberButton />
         <OperatorButton />
-        <SpecialButton />
+        
       </div>
     </div>
   );
