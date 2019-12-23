ce8f5cc56558ac7a9e08c81638a6f41e6f86d016
Blair <abrgroup@gmail.com>
2019-12-22 19:01:28 -0500

    MVP complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./App.css";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
+import Display from "./components/DisplayComponents/Display";
+
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -16,6 +21,16 @@ function App() {
   return (
     <div className="container">
       <Logo />
+      <Display />
+      <div className="buttonStuff">
+        <div className="innerContent">
+          <Specials />
+          <Numbers />
+        </div>
+        <div className="rightContent">
+          <Operators />
+        </div>
+      </div>
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
       </div>
