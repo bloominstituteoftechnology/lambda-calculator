f858dac9a51171768a627a07ae7d016420ae4460
Erin Davis <davisaerin@gmail.com>
2020-01-04 16:22:32 -0600

    need to update branch to work from elsewhere

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
+import { numbers } from "../../../data.js";
 
 //import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
-/* 
-import { numbers } from '../../../data' 
+// example of import from data.js. Note all the ../   This is how we move through folders.
+/*
+import { numbers } from '../../../data'
 */
 //Import your array data to from the provided data file
 
