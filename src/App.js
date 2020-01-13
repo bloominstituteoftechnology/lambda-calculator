6bb5fcdac3bff12b93f8374cd6312fa74ff35a52
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-01-13 14:21:25 -0800

    fixed margin

--- src/App.js
+++ src/App.js
@@ -28,10 +28,10 @@ function App() {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
       <div className="horizontal_buttons">
-        <Specials/>
-        <Numbers/>
+        <Specials />
+        <Numbers />
       </div>  
-        <Operators/>
+        <Operators />
         
         
   
--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -2,15 +2,14 @@ import React from "react";
 
 
 const NumberButton = (props) => {
-  console.log(props);
   return (
     <>
-      <button className = {props.numberProp == 0 ? "number_button zero" : "number_button"}
-      // className="number_button"
-      //onClick={() => props.setNumberState(props.numberState)}      
+      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button className="buttons numberButton"
+      // onClick ={() => props.setNumberState(props.num)}
       >
-      {props.numberProp} 
-     </button>      
+      {props.displayNum}
+      </button>
     </>
   );
 };
